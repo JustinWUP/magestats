92577aa7ceacd7443bd3c86bca524ebe0bf2accf
Justin Bean <justinb@plumtreegroup.net>
2013-04-23 14:59:33 -0500

    getting a template together for the admin page

--- app/code/community/Jb/Stats/controllers/IndexController.php
+++ app/code/community/Jb/Stats/controllers/IndexController.php
@@ -5,8 +5,8 @@ class Jb_Stats_IndexController extends Mage_Adminhtml_Controller_Action
     public function indexAction()
     {
         $this->loadLayout();
+       // $this->getLayout()->getBlock('content')->append($this->getLayout()->createBlock('twits/helloWorld'));
         $this->renderLayout();
-        echo "hey sup";
     }
 }
 ?>
